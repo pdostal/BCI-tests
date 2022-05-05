04b6e72f49b8690b8f54a0a53cd8d7f75b53b4ba
Jose Lausuch <jlausuch@gmail.com>
2022-05-05 21:03:21 +0200

    Enable tests for openjdk-17 container

--- tests/test_openjdk.py
+++ tests/test_openjdk.py
@@ -2,14 +2,26 @@
 import pytest
 
 from bci_tester.data import OPENJDK_11_CONTAINER
+from bci_tester.data import OPENJDK_17_CONTAINER
+
+CONTAINER_IMAGES = [
+    OPENJDK_11_CONTAINER,
+    OPENJDK_17_CONTAINER,
+]
 
 
 @pytest.mark.parametrize(
     "container,java_version",
-    [(OPENJDK_11_CONTAINER, "11")],
+    [
+        pytest.param(cont, ver, marks=cont.marks)
+        for cont, ver in (
+            (OPENJDK_11_CONTAINER, "11"),
+            (OPENJDK_17_CONTAINER, "17"),
+        )
+    ],
     indirect=["container"],
 )
-def test_jdk_version(container, java_version):
+def test_jdk_version(container, java_version: str):
     """Check that the environment variable ``JAVA_VERSION`` is equal to the output
     of :command:`java --version`.
 
